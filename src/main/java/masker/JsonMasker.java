6a207afe642e8f6e0460c5db5c05e3bf787a0e2d
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2021-12-04 12:10:22 +0100

    Added curly brackets for readability

--- src/main/java/masker/JsonMasker.java
+++ src/main/java/masker/JsonMasker.java
@@ -119,14 +119,18 @@ final class JsonMasker extends AbstractMasker {
         byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
         int i = 0;
         while (i < inputBytes.length - 2) {
-            if (inputBytes[i] != UTF8Encoding.COLON.getUtf8ByteValue())
+            if (inputBytes[i] != UTF8Encoding.COLON.getUtf8ByteValue()) {
                 i++; // loop until index is on colon
+            }
             int colonIndex = i;
-            while (inputBytes[i] != UTF8Encoding.DOUBLE_QUOTE.getUtf8ByteValue())
+            i--; // step back from colon
+            while (inputBytes[i] != UTF8Encoding.DOUBLE_QUOTE.getUtf8ByteValue()) {
                 i--; // loop back until index is on closing quote of key
+            }
             int closingQuoteIndex = i;
-            while (inputBytes[i] != UTF8Encoding.DOUBLE_QUOTE.getUtf8ByteValue())
+            while (inputBytes[i] != UTF8Encoding.DOUBLE_QUOTE.getUtf8ByteValue()) {
                 i--; // loop back until index is on opening quote of key
+            }
             int openingQuoteIndex = i;
             int keyLength = closingQuoteIndex - openingQuoteIndex;
             byte[] keyBytes = new byte[keyLength];
@@ -137,8 +141,9 @@ final class JsonMasker extends AbstractMasker {
                 i = i + 5; // +5 since minimum amount of characters between colon is 5 --> {"a":1,"b":2}
                 continue;
             }
-            while (inputBytes[i] != UTF8Encoding.DOUBLE_QUOTE.getUtf8ByteValue())
+            while (inputBytes[i] != UTF8Encoding.DOUBLE_QUOTE.getUtf8ByteValue()) {
                 i++; // loop until index is on opening quote of value
+            }
             i++; // step over quote
             int targetValueLength = 0;
             while(inputBytes[i] != UTF8Encoding.DOUBLE_QUOTE.getUtf8ByteValue()) {

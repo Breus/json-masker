cec0cb39c7754e2b496655dfa114a6767a189373
Arthur Gavlyukovskiy <agavlyukovskiy@gmail.com>
2023-12-21 01:05:01 +0100

    Added regex benchmark, split benchmarks into baseline and json-masker, added parameter with character set to test how well json-masker is handling resizes

---
+++ src/jmh/java/dev/blaauwendraad/masker/json/BaselineBenchmark.java
@@ -0,0 +1,88 @@
+package dev.blaauwendraad.masker.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.blaauwendraad.masker.json.config.JsonMaskingConfig;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@Warmup(iterations = 1, time = 3)
+@Fork(value = 1)
+@Measurement(iterations = 1, time = 3)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@BenchmarkMode(Mode.Throughput)
+public class BaselineBenchmark {
+
+    @org.openjdk.jmh.annotations.State(Scope.Thread)
+    public static class State {
+        @Param({"1kb", "128kb", "2mb"})
+        String jsonSize;
+        @Param({"unicode"})
+        String characters;
+        @Param({"0.01"})
+        double maskedKeyProbability;
+
+        private Set<String> targetKeys;
+        private String jsonString;
+        private byte[] jsonBytes;
+        private ObjectMapper objectMapper;
+        private List<Pattern> regexList;
+
+        @Setup
+        public synchronized void setup() {
+            targetKeys = BenchmarkUtils.getTargetKeys(20);
+            jsonString = BenchmarkUtils.randomJson(targetKeys, jsonSize, characters, maskedKeyProbability);
+            jsonBytes = jsonString.getBytes(StandardCharsets.UTF_8);
+
+            objectMapper = new ObjectMapper();
+
+            regexList = targetKeys.stream()
+                    // will only match primitive values, not objects or arrays, but it's good to show the difference
+                    .map(key -> Pattern.compile("(\"" + key + "\"\\s*:\\s*)(\"?[^\"]*\"?)", Pattern.CASE_INSENSITIVE))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Benchmark
+    public int countBytes(State state) {
+        int sum = 0;
+        for (int i = 0; i < state.jsonBytes.length; i++) {
+            sum += state.jsonBytes[i];
+        }
+        return sum;
+    }
+
+    @Benchmark
+    public String regexReplace(State state) {
+        String masked = state.jsonString;
+        for (Pattern pattern : state.regexList) {
+            Matcher matcher = pattern.matcher(masked);
+            if (matcher.find()) {
+                masked = matcher.replaceAll(matchResult -> {
+                    String beforeValuePart = matchResult.group(1);
+                    String value = matchResult.group(2);
+                    int maskCount = value.startsWith("\"") ? value.length() - 2 : value.length();
+                    return beforeValuePart + "*".repeat(maskCount);
+                });
+            }
+        }
+        return masked;
+    }
+
+    @Benchmark
+    public String jacksonParseAndMask(State state) throws IOException {
+        return ParseAndMaskUtil.mask(
+                state.jsonString,
+                state.targetKeys,
+                JsonMaskingConfig.TargetKeyMode.MASK,
+                state.objectMapper
+        ).toString();
+    }
+}
--- src/jmh/java/dev/blaauwendraad/masker/json/JsonMaskerBenchmark.java
+++ src/jmh/java/dev/blaauwendraad/masker/json/JsonMaskerBenchmark.java
@@ -1,6 +1,5 @@
 package dev.blaauwendraad.masker.json;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.blaauwendraad.masker.json.config.JsonMaskingConfig;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -12,18 +11,11 @@ import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.Warmup;
-import randomgen.json.RandomJsonGenerator;
-import randomgen.json.RandomJsonGeneratorConfig;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
-import static randomgen.json.JsonStringCharacters.getPrintableAsciiCharacters;
 
 @Warmup(iterations = 1, time = 3)
 @Fork(value = 1)
@@ -34,71 +26,34 @@ public class JsonMaskerBenchmark {
 
     @org.openjdk.jmh.annotations.State(Scope.Thread)
     public static class State {
-        @Param({ "1kb", "128kb", "2mb" })
+        @Param({"1kb", "128kb", "2mb"})
         String jsonSize;
-        @Param({ "0.01", "0.1" })
+        @Param({"ascii (no quote)", "ascii", "unicode"})
+        String characters;
+        @Param({"0.01", "0.1"})
         double maskedKeyProbability;
-        @Param({ "none", "8" })
+        @Param({"none", "8"})
         String obfuscationLength;
+
         private String jsonString;
         private byte[] jsonBytes;
         private JsonMasker jsonMasker;
-        private ObjectMapper objectMapper;
 
         @Setup
         public synchronized void setup() {
-            Set<String> keysToBeMasked = getTargetKeys();
+            Set<String> targetKeys = BenchmarkUtils.getTargetKeys(20);
 
-            RandomJsonGeneratorConfig config = RandomJsonGeneratorConfig.builder()
-                    .setAllowedCharacters(
-                            getPrintableAsciiCharacters().stream()
-                                    .filter(c -> c != '"')
-                                    .collect(Collectors.toSet())
-                    )
-                    .setTargetKeys(keysToBeMasked)
-                    .setTargetKeyPercentage(maskedKeyProbability)
-                    .setTargetJsonSizeBytes(BenchmarkUtils.parseSize(jsonSize))
-                    .createConfig();
-
-            jsonString = new RandomJsonGenerator(config).createRandomJsonNode().toString();
+            jsonString = BenchmarkUtils.randomJson(targetKeys, jsonSize, characters, maskedKeyProbability);
             jsonBytes = jsonString.getBytes(StandardCharsets.UTF_8);
 
             jsonMasker = JsonMasker.getMasker(
-                    JsonMaskingConfig.custom(keysToBeMasked, JsonMaskingConfig.TargetKeyMode.MASK)
+                    JsonMaskingConfig.custom(targetKeys, JsonMaskingConfig.TargetKeyMode.MASK)
                             .obfuscationLength(Objects.equals(obfuscationLength, "none")
-                                                       ? -1
-                                                       : Integer.parseInt(obfuscationLength))
+                                    ? -1
+                                    : Integer.parseInt(obfuscationLength))
                             .build()
             );
-            objectMapper = new ObjectMapper();
         }
-
-        private Set<String> getTargetKeys() {
-            Set<String> targetKeys = new HashSet<>();
-            for (int i = 0; i < 20; i++) {
-                targetKeys.add("someSecret" + i);
-            }
-            return targetKeys;
-        }
-    }
-
-    @Benchmark
-    public int baselineCountBytes(State state) {
-        int sum = 0;
-        for (int i = 0; i < state.jsonBytes.length; i++) {
-            sum += state.jsonBytes[i];
-        }
-        return sum;
-    }
-
-    @Benchmark
-    public String jacksonString(State state) throws IOException {
-        return ParseAndMaskUtil.mask(
-                state.jsonString,
-                state.getTargetKeys(),
-                JsonMaskingConfig.TargetKeyMode.MASK,
-                state.objectMapper
-        ).toString();
     }
 
     @Benchmark

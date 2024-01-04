49d1d42855c438645548228fa2f349b47b0b88c5
Arthur Gavlyukovskiy <agavlyukovskiy@gmail.com>
2024-01-04 08:32:48 +0100

    Store replacement operations in order to do a single array resize (#33)
    * Store replacement operations in order to do a single array resize
    
    * Move toString to after all getters
    
    * Do not replace bytes in place
    
    * Remove now unused method
    
    * Use final for the replacement operations
    
    * Also refactor a bit string value masking
    
    * Added benchmark results
    
    * Address review comments

--- src/main/java/dev/blaauwendraad/masker/json/MaskingState.java
+++ src/main/java/dev/blaauwendraad/masker/json/MaskingState.java
@@ -1,5 +1,8 @@
 package dev.blaauwendraad.masker.json;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents the state of the {@link JsonMasker} at a given point in time during the {@link JsonMasker#mask(byte[])}
  * operation.
@@ -7,6 +10,8 @@ package dev.blaauwendraad.masker.json;
 public final class MaskingState {
     private byte[] message;
     private int currentIndex;
+    private final List<ReplacementOperation> replacementOperations = new ArrayList<>();
+    private int replacementOperationsTotalDifference = 0;
 
     public MaskingState(byte[] message, int currentIndex) {
         this.message = message;
@@ -29,10 +34,6 @@ public final class MaskingState {
         return message[currentIndex];
     }
 
-    public void setByteAtCurrentIndex(byte newByte) {
-        message[currentIndex] = newByte;
-    }
-
     public byte byteAtCurrentIndexMinusOne() {
         return message[currentIndex - 1];
     }
@@ -49,10 +50,55 @@ public final class MaskingState {
         return message;
     }
 
+    /**
+     * Adds new delayed replacement operation to the list of operations to be applied to the message.
+     */
+    public void addReplacementOperation(int startIndex, int endIndex, int maskLength, byte maskByte) {
+        ReplacementOperation replacementOperation = new ReplacementOperation(startIndex, endIndex, maskLength, maskByte);
+        replacementOperations.add(replacementOperation);
+        replacementOperationsTotalDifference += replacementOperation.difference();
+    }
+
+    /**
+     * Returns the list of replacement operations that need to be applied to the message.
+     */
+    public List<ReplacementOperation> getReplacementOperations() {
+        return replacementOperations;
+    }
+
+    /**
+     * Returns the total difference between the masks and target values lengths of all replacement operations.
+     */
+    public int getReplacementOperationsTotalDifference() {
+        return replacementOperationsTotalDifference;
+    }
+
     // for debugging purposes, shows the current state of message traversal
-    public String peek() {
+    @Override
+    public String toString() {
         return "current: '" + (currentIndex == message.length ? "<end of json>" : (char) message[currentIndex]) + "'," +
                 " before: '" + new String(message, Math.max(0, currentIndex - 10), Math.min(10, currentIndex)) + "'," +
                 " after: '" + new String(message, currentIndex, Math.min(10, message.length - currentIndex)) + "'";
+    }
+
+    /**
+     * Represents a delayed replacement that requires resizing of the message byte array. In order to avoid resizing on
+     * every mask, we store the replacement operations in a list and apply them all at once at the end, thus making only
+     * a single resize operation.
+     *
+     * @param startIndex index from which to start replacing
+     * @param endIndex   index at which to stop replacing
+     * @param maskLength length of the mask to apply
+     * @param maskByte   byte to use for the mask
+     */
+    public record ReplacementOperation(int startIndex, int endIndex, int maskLength, byte maskByte) {
+
+        /**
+         * The difference between the mask length and the length of the target value to replace.
+         * Used to calculate keep track of the offset during replacements.
+         */
+        public int difference() {
+            return maskLength - (endIndex - startIndex);
+        }
     }
 }

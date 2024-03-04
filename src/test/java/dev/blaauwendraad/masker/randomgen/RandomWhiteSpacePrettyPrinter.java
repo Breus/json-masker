35a5c8f5a12c9bbb9474f7b59044087cc578a01a
Alexey Shary <badirbis@yandex.ru>
2024-03-04 17:00:01 +0300

    Rework NoFailingExecutionFuzzingTest.executeTest to generate JsonNode instead of byte[] and rework related classes

--- src/test/java/dev/blaauwendraad/masker/randomgen/RandomWhiteSpacePrettyPrinter.java
+++ src/test/java/dev/blaauwendraad/masker/randomgen/RandomWhiteSpacePrettyPrinter.java
@@ -28,54 +28,63 @@ public class RandomWhiteSpacePrettyPrinter implements PrettyPrinter {
 
     @Override
     public void writeStartObject(JsonGenerator gen) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.writeStartObject(gen);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void writeEndObject(JsonGenerator gen, int nrOfEntries) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.writeEndObject(gen, nrOfEntries);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void writeObjectEntrySeparator(JsonGenerator gen) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.writeObjectEntrySeparator(gen);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void writeObjectFieldValueSeparator(JsonGenerator gen) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.writeObjectFieldValueSeparator(gen);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void writeStartArray(JsonGenerator gen) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.writeStartArray(gen);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void writeEndArray(JsonGenerator gen, int nrOfValues) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.writeEndArray(gen, nrOfValues);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void writeArrayValueSeparator(JsonGenerator gen) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.writeArrayValueSeparator(gen);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void beforeArrayValues(JsonGenerator gen) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.beforeArrayValues(gen);
         addRandomWhiteSpace(gen);
     }
 
     @Override
     public void beforeObjectEntries(JsonGenerator gen) throws IOException {
+        addRandomWhiteSpace(gen);
         defaultPrettyPrinter.beforeObjectEntries(gen);
         addRandomWhiteSpace(gen);
     }
@@ -85,9 +94,9 @@ public class RandomWhiteSpacePrettyPrinter implements PrettyPrinter {
             return;
         }
 
-        // we use nextInt(6) and randomInt < 3 to make there a 50% chance of setting white space
-        int randomInt = ThreadLocalRandom.current().nextInt(6);
-        if (randomInt < 3) {
+        // we use nextInt(8) and randomInt < 4 to make there a 50% chance of setting white space
+        int randomInt = ThreadLocalRandom.current().nextInt(8);
+        if (randomInt < 4) {
             gen.writeRaw((char) getRandomWhiteSpaceByte(randomInt));
             numberOfWhiteSpacesToInject--;
         }
@@ -98,6 +107,7 @@ public class RandomWhiteSpacePrettyPrinter implements PrettyPrinter {
             case 0 -> HORIZONTAL_TAB.getAsciiByteValue();
             case 1 -> LINE_FEED.getAsciiByteValue();
             case 2 -> SPACE.getAsciiByteValue();
+            case 3 -> CARRIAGE_RETURN.getAsciiByteValue();
             default -> throw new IllegalStateException("Unexpected value to get random whitespace byte: " + randomInt);
         };
     }

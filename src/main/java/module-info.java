73891d7d4be0d93d351f0213e1da9c4215c61dfa
Artur Havliukovskyi <agavlyukovskiy@gmail.com>
2024-04-13 00:51:30 +0200

    Use 'requires static'

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module dev.blaauwendraad.masker.json {
     requires java.base;
-    requires org.jspecify;
+    // https://github.com/ben-manes/caffeine/issues/535#issuecomment-854879514
+    requires static org.jspecify;
 
     exports dev.blaauwendraad.masker.json;
     exports dev.blaauwendraad.masker.json.config;

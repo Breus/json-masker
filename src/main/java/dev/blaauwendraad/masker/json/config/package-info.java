a310f1906be78042c2b7ae109506c7343d517059
Artur Havliukovskyi <agavlyukovskiy@gmail.com>
2024-04-13 10:42:55 +0200

    Added javadoc on package-info

--- src/main/java/dev/blaauwendraad/masker/json/config/package-info.java
+++ src/main/java/dev/blaauwendraad/masker/json/config/package-info.java
@@ -1,3 +1,7 @@
+/**
+ * This package contains configuration and builders for creating a {@link dev.blaauwendraad.masker.json.JsonMasker}
+ * instance
+ */
 @ParametersAreNonnullByDefault
 package dev.blaauwendraad.masker.json.config;
 
--- src/main/java/dev/blaauwendraad/masker/json/package-info.java
+++ src/main/java/dev/blaauwendraad/masker/json/package-info.java
@@ -1,3 +1,8 @@
+/**
+ * This package contains the {@link dev.blaauwendraad.masker.json.JsonMasker}, main entrypoint for masking the JSON.
+ * Additionally, the package contains {@link dev.blaauwendraad.masker.json.ValueMasker} for low-level masking
+ * customization and {@link dev.blaauwendraad.masker.json.ValueMaskers} for out-of-the-box maskers.
+ */
 @ParametersAreNonnullByDefault
 package dev.blaauwendraad.masker.json;
 

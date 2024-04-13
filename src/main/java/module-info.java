ad7680ddbcb6591e29b1191ba4e3c966ce563f7d
Artur Havliukovskyi <agavlyukovskiy@gmail.com>
2024-04-13 10:46:24 +0200

    Revert module-info.java (moved to separate PR)

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module dev.blaauwendraad.masker.json {
-    requires java.base;
-    // https://github.com/ben-manes/caffeine/issues/535#issuecomment-854879514
-    requires static org.jspecify;
-
-    exports dev.blaauwendraad.masker.json;
-    exports dev.blaauwendraad.masker.json.config;
-    exports dev.blaauwendraad.masker.json.path;
-}

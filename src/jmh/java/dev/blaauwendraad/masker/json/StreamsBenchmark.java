ead9c88fa81cc309a34f084b3545fe90f278d468
donavdey <donavdey@gmail.com>
2024-08-05 18:52:15 +0200

    Remove piped streams from StreamsBenchmark as the measurement error is too high

--- src/jmh/java/dev/blaauwendraad/masker/json/StreamsBenchmark.java
+++ src/jmh/java/dev/blaauwendraad/masker/json/StreamsBenchmark.java
@@ -6,7 +6,6 @@ import org.jspecify.annotations.NullUnmarked;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
@@ -18,22 +17,16 @@ import org.openjdk.jmh.annotations.Warmup;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @Warmup(iterations = 1, time = 3)
@@ -43,15 +36,16 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.Throughput)
 public class StreamsBenchmark {
 
+    static final String INPUT_FILE_STREAM_NAME = "input.json";
+    static final String OUTPUT_FILE_STREAM_NAME = "output.json";
+
+
     @org.openjdk.jmh.annotations.State(Scope.Thread)
     @NullUnmarked
     public static class State {
-        static final String INPUT_FILE_STREAM_NAME = "input.json";
-        static final String OUTPUT_FILE_STREAM_NAME = "output.json";
-
-        @Param({"ByteArrayStream", "FileStream", "PipedStream"})
+        @Param({"ByteArrayStream", "FileStream"})
         String streamInputType;
-        @Param({"ByteArrayStream", "FileStream", "PipedStream"})
+        @Param({"ByteArrayStream", "FileStream"})
         String streamOutputType;
         @Param({"2mb"})
         String jsonSize;
@@ -61,19 +55,15 @@ public class StreamsBenchmark {
         double maskedKeyProbability;
         @Param({"true"})
         boolean jsonPath;
+        byte[] json;
 
-        private InputStream inputStream;
-        private OutputStream outputStream;
-        private Closeable pipeConsumer;
-        private Closeable pipeProducer;
-        private ExecutorService executor;
         private JsonMasker jsonMasker;
 
-        @Setup(Level.Invocation) // streams need to be reset for each benchmark method execution
+        @Setup
         public synchronized void setup() throws IOException {
             // prepare a json
             Set<String> targetKeys = BenchmarkUtils.getTargetKeys(20);
-            byte[] json = BenchmarkUtils.randomJson(targetKeys, jsonSize, characters, maskedKeyProbability).getBytes(StandardCharsets.UTF_8);
+            json = BenchmarkUtils.randomJson(targetKeys, jsonSize, characters, maskedKeyProbability).getBytes(StandardCharsets.UTF_8);
 
             // prepare a file for FileStreams
             FileWriter inputFileWriter = new FileWriter(INPUT_FILE_STREAM_NAME);
@@ -81,12 +71,7 @@ public class StreamsBenchmark {
             inputFileWriter.flush();
             inputFileWriter.close();
 
-            executor = Executors.newFixedThreadPool(2);
-
-            // create streams
-            inputStream = createInputStream(json, streamInputType);
-            outputStream = createOutputStream(streamOutputType);
-
+            // create a masker
             JsonMaskingConfig.Builder builder = JsonMaskingConfig.builder();
             if (jsonPath) {
                 builder.maskJsonPaths(JsonPathTestUtils.transformToJsonPathKeys(targetKeys, new String(json, StandardCharsets.UTF_8)));
@@ -96,73 +81,37 @@ public class StreamsBenchmark {
             jsonMasker = JsonMasker.getMasker(builder.build());
         }
 
-        @TearDown(Level.Invocation) // streams need to be reset for each benchmark method execution
+        @TearDown
         public synchronized void tearDown() throws IOException {
-            executor.shutdownNow();
-            if (pipeConsumer != null) {
-                pipeConsumer.close();
-            }
-            if (pipeProducer != null) {
-                pipeProducer.close();
-            }
-            inputStream.close();
-            outputStream.close();
-            Files.deleteIfExists(Path.of(State.INPUT_FILE_STREAM_NAME));
-            Files.deleteIfExists(Path.of(State.OUTPUT_FILE_STREAM_NAME));
-        }
-
-        private InputStream createInputStream(byte[] json, String inputStreamType) throws IOException {
-            return switch (inputStreamType) {
-                case "ByteArrayStream" -> new ByteArrayInputStream(json);
-                case "FileStream" -> new FileInputStream(INPUT_FILE_STREAM_NAME);
-                case "PipedStream" -> {
-                    PipedOutputStream consumerPipedOutputStream = new PipedOutputStream(); // NOSONAR it is closed in the other method
-                    PipedInputStream consumerPipedInputStream = new PipedInputStream(consumerPipedOutputStream); // NOSONAR it is closed in the other method
-                    executor.submit(() -> {
-                        try {
-                            consumerPipedOutputStream.write(json);
-                            consumerPipedOutputStream.flush();
-                            consumerPipedOutputStream.close();
-                        } catch (IOException e) {
-                            throw new UncheckedIOException(e);
-                        }
-                    });
-                    pipeProducer = consumerPipedOutputStream;
-                    yield consumerPipedInputStream;
-                }
-                default -> throw new IllegalArgumentException("Unknown stream type");
-            };
-        }
-
-        private OutputStream createOutputStream(String outputStreamType) throws IOException {
-            return switch (outputStreamType) {
-                case "ByteArrayStream" -> new ByteArrayOutputStream();
-                case "FileStream" -> new FileOutputStream(OUTPUT_FILE_STREAM_NAME);
-                case "PipedStream" -> {
-                    PipedOutputStream consumerPipedOutputStream = new PipedOutputStream(); // NOSONAR it is closed in the other method
-                    PipedInputStream consumerPipedInputStream = new PipedInputStream(consumerPipedOutputStream); // NOSONAR it is closed in the other method
-                    executor.submit(() -> {
-                        try {
-                            while (true) {
-                                consumerPipedInputStream.readAllBytes();
-                                Thread.sleep(1000);
-                            }
-                        } catch (IOException e) {
-                            throw new UncheckedIOException(e);
-                        } catch (InterruptedException e) {
-                            // finish the execution
-                        }
-                    });
-                    pipeConsumer = consumerPipedInputStream;
-                    yield consumerPipedOutputStream;
-                }
-                default -> throw new IllegalArgumentException("Unknown stream type");
-            };
+            Files.deleteIfExists(Path.of(INPUT_FILE_STREAM_NAME));
+            Files.deleteIfExists(Path.of(OUTPUT_FILE_STREAM_NAME));
         }
     }
 
+    private InputStream createInputStream(byte[] json, String inputStreamType) throws IOException {
+        return switch (inputStreamType) {
+            case "ByteArrayStream" -> new ByteArrayInputStream(json);
+            case "FileStream" -> new FileInputStream(INPUT_FILE_STREAM_NAME);
+            default -> throw new IllegalArgumentException("Unknown stream type");
+        };
+    }
+
+    private OutputStream createOutputStream(String outputStreamType) throws IOException {
+        return switch (outputStreamType) {
+            case "ByteArrayStream" -> new ByteArrayOutputStream();
+            case "FileStream" -> new FileOutputStream(OUTPUT_FILE_STREAM_NAME);
+            default -> throw new IllegalArgumentException("Unknown stream type");
+        };
+    }
+
     @Benchmark
-    public void jsonMaskerStreams(State state) {
-        state.jsonMasker.mask(state.inputStream, state.outputStream);
+    public void jsonMaskerStreams(State state) throws IOException {
+        InputStream inputStream = createInputStream(state.json, state.streamInputType);
+        OutputStream outputStream = createOutputStream(state.streamOutputType);
+
+        state.jsonMasker.mask(inputStream, outputStream);
+
+        inputStream.close();
+        outputStream.close();
     }
 }
